d61cc5c53c719f0cfe502ae5cf54b13d10f39de1
Ian Blake <ian@ianbl8.com>
2023-10-30 10:50:01 +0000

    New packages created, and MusicBoxActivity moved to activities package

--- app/src/main/java/com/ianbl8/musicbox/activities/MusicBoxActivity.kt
+++ app/src/main/java/com/ianbl8/musicbox/activities/MusicBoxActivity.kt
@@ -1,7 +1,8 @@
-package com.ianbl8.musicbox
+package com.ianbl8.musicbox.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ianbl8.musicbox.R
 
 class MusicBoxActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
