b3851d15fd557b368f998521c3a62ab326cd118e
Ian Blake <ian@ianbl8.com>
2023-10-30 10:36:29 +0000

    Initial project template

---
+++ app/src/main/java/com/ianbl8/musicbox/MusicBoxActivity.kt
@@ -0,0 +1,11 @@
+package com.ianbl8.musicbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MusicBoxActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_musicbox)
+    }
+}
